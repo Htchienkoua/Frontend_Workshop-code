6423cb45d8b55ca493c08f6f3636e21f18f57d3d
Jurgis Bridžius <jurgis.bridzius@swedbank.lt>
2023-03-28 11:28:20 +0300

    task-2: added routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { PostsComponent } from './posts/posts.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/posts' },
+  {
+    path: 'posts',
+    component: PostsComponent,
+  },
+  {
+    path: '**',
+    component: PostsComponent,
+  },
+];
 
 @NgModule({
   declarations: [AppComponent, PostsComponent],
-  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

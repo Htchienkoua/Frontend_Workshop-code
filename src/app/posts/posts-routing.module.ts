c4c3f33189684c61f889e0273df26379625d6fc4
Jurgis Bridžius <jurgis.bridzius@swedbank.lt>
2023-03-28 14:48:39 +0300

    task-7: detailed post component

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailedPostComponent } from './detailed-post/detailed-post.component';
 import { PostsComponent } from './posts.component';
 
-const routes: Routes = [{ path: '', component: PostsComponent }];
+const routes: Routes = [
+  { path: '', component: PostsComponent },
+  { path: ':id', component: DetailedPostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -5,9 +5,10 @@ import { PostsRoutingModule } from './posts-routing.module';
 import { FilterPostsComponent } from './filter-posts/filter-posts.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PostsComponent } from './posts.component';
+import { DetailedPostComponent } from './detailed-post/detailed-post.component';
 
 @NgModule({
   imports: [CommonModule, PostsRoutingModule, ReactiveFormsModule],
-  declarations: [FilterPostsComponent, PostsComponent],
+  declarations: [FilterPostsComponent, PostsComponent, DetailedPostComponent],
 })
 export class PostsModule {}

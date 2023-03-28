d7f8ee0706bd5e70027fe5e3f43de37b9287c82d
Jurgis Bridžius <jurgis.bridzius@swedbank.lt>
2023-03-28 15:35:41 +0300

    task-9: added functional and class-based available-post guards

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AvailablePostGuard } from '../guards/available-post.guard';
+import { functionalAvailablePostGuard } from '../guards/functional-available-post.guard';
 import { DetailedPostComponent } from './detailed-post/detailed-post.component';
 import { PostsComponent } from './posts.component';
 
 const routes: Routes = [
   { path: '', component: PostsComponent },
-  { path: ':id', component: DetailedPostComponent },
+  {
+    path: ':id',
+    component: DetailedPostComponent,
+    canActivate: [functionalAvailablePostGuard],
+  },
 ];
 
 @NgModule({

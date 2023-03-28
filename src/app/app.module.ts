925d8297ea12979b0973ddf7bd88143bf7eed9be
Jurgis Bridžius <jurgis.bridzius@swedbank.lt>
2023-03-28 10:02:52 +0300

    task-1: posts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostsComponent],
   imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
